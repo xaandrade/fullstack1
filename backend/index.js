382a808c0fff96a8f3285f3eb75a6da00390e47b
xaandrade <151124274+xaandrade@users.noreply.github.com>
2026-04-08 19:40:28 -0500

    first upload

---
+++ backend/index.js
@@ -0,0 +1,42 @@
+
+const express = require('express');
+const fs = require('fs');
+const cors = require('cors');
+
+const app = express();
+const PORT = 3000;
+
+app.use(cors());
+app.use(express.json());
+
+const dbFile = './tasks.json';
+
+
+if (!fs.existsSync(dbFile)) {
+  fs.writeFileSync(dbFile, JSON.stringify([]));
+}
+
+
+app.get('/tasks', (req, res) => {
+  const tasks = JSON.parse(fs.readFileSync(dbFile));
+  res.json(tasks);
+});
+
+
+app.post('/tasks', (req, res) => {
+  const tasks = JSON.parse(fs.readFileSync(dbFile));
+  const newTask = { id: Date.now(), title: req.body.title };
+  tasks.push(newTask);
+  fs.writeFileSync(dbFile, JSON.stringify(tasks));
+  res.json(newTask);
+});
+
+
+app.delete('/tasks/:id', (req, res) => {
+  let tasks = JSON.parse(fs.readFileSync(dbFile));
+  tasks = tasks.filter(t => t.id != req.params.id);
+  fs.writeFileSync(dbFile, JSON.stringify(tasks));
+  res.json({ message: 'Tarea eliminada' });
+});
+
+app.listen(PORT, () => console.log(`Backend corriendo en http://localhost:${PORT}`));
---
+++ database/init.sql
@@ -0,0 +1,4 @@
+CREATE TABLE IF NOT EXISTS tasks (
+  id SERIAL PRIMARY KEY,
+  title TEXT
+);
